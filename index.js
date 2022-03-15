b57b3fc40e130a4c974ca6ca55c913bc80a1c29e
willianmarquess <willian.goncalves4@gmail.com>
2022-03-15 17:02:10 -0300

    Revert "chage test to pass null"
    This reverts commit 93965489e6ab693ca58240e05ca5d7160550a971.

--- index.js
+++ index.js
@@ -14,10 +14,7 @@ object = {
 
 
 const main = (object) => {
-  const { body: { email, password }} = object;
-  if (email === null && object.body === null) return false;
-  if (email.length > 5 && password.length > 5) return true
-  return false;
-}
+
+};
 
 module.exports = main;
