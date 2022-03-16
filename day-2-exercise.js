87ffe42a150262ec9388408d93e63ca82744f6d4
nato-re <alvares.renato@hotmail.com>
2022-03-16 13:31:46 -0300

    day2-exercises

---
+++ day-2-exercise.js
@@ -0,0 +1,25 @@
+/* 
+Dada a função `main` que recebe um parâmetro, como abaixo, verifique se todos os objetos do array dentro de body possuem a propriedade `name` como uma `string` e quantidade é um número.
+Sua função `main` deve retornar um booleano (true e false), `true` caso **todos** os objetos dentro do array `body` tenham `name` como string e `quantity` seja maior que 0.
+
+param = {
+  body: [
+    { name: 'string', quantity: 1 },
+    { name: 'outra string', quantity: 0 },
+    { name: 0, quantity: 21 },
+    { name: NaN, quantity: -1 }
+  ]
+}
+retorna [true, false, false, false]
+*/
+
+const callback = (objeto) => {
+  return typeof objeto.name === 'string' && objeto.quantity > 0 
+} 
+
+const main = (param) => {
+  return param.body.every(callback)
+};
+
+
+module.exports = main;
